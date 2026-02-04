a9ef2590a4e9a7b9ab7b86327886591e418fda91
test <test@test.com>
2026-02-04 11:49:40 -0600

    Daily automated commit - 2026-02-04 11:49:36

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,12 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('daily:upload')->dailyAt('02:00');
+
+Schedule::command('shipment:create',
+[
+	'--start-date' => now()->toDateTimeString(),
+])->dailyAt('01:00')
+->sendOutputTo(storage_path('logs/commands.log'));
+
+Schedule::command('daily:upload')->dailyAt('02:00')
+->sendOutputTo(storage_path('logs/commands.log'));
