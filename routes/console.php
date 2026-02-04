82660242a5190fd3fd3b06c3c539f40320606f88
test <test@test.com>
2026-02-04 13:34:02 -0600

    Daily automated commit - 2026-02-04 13:34:02

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Schedule::command('shipment:create',['--start-date' => now()->toDateTimeString()
 Schedule::command('daily:upload')
 	->everyMinute()
 	//->dailyAt('12:50:00')
-	->appendOutputTo(storage_path('logs/schedule-commands.log'));
+	->appendOutputTo(storage_path('logs/scheduled-commands.log'));
