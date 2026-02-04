1e23598a0880cf438a15d48395e24f45d6e5837f
test <test@test.com>
2026-02-04 12:48:23 -0600

    Daily automated commit - 2026-02-04 12:48:22

--- routes/console.php
+++ routes/console.php
@@ -3,11 +3,27 @@
 use Illuminate\Support\Facades\Schedule;
 
 
-Schedule::command('shipment:create',
-[
-	'--start-date' => now()->toDateTimeString(),
-])->dailyAt('01:00')
-->appendOutputTo(storage_path('logs/commands.log'));
+Schedule::command('shipment:create',['--start-date' => now()->toDateTimeString()])
+	//->dailyAt('01:00')
+	->appendOutputTo(storage_path('logs/commands.log'))
+	->onSuccess(function ()
+	{
+		file_put_contents(storage_path('logs/test.log'), 'ran at ' . now() . "\n", FILE_APPEND);
+	})
+	->onFailure(function ()
+        {
+                file_put_contents(storage_path('logs/test.log'), 'error at ' . now() . "\n", FILE_APPEND);
+        });
 
-Schedule::command('daily:upload')->dailyAt('12:00')
-->appendOutputTo(storage_path('logs/commands.log'));
+
+Schedule::command('daily:upload')
+	//->dailyAt('12:46')
+	->appendOutputTo(storage_path('logs/commands.log'))
+	->onSuccess(function ()
+        {
+                file_put_contents(storage_path('logs/test.log'), 'ran at ' . now() . "\n", FILE_APPEND);
+        })
+	->onFailure(function ()
+        {
+                file_put_contents(storage_path('logs/test.log'), 'error at ' . now() . "\n", FILE_APPEND);
+        });
